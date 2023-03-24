6f5ed7216c15c7f01d05f351532e88da475795b9
Nando <Nando@DESKTOP-ELMP60R>
2023-03-23 22:18:44 -0500

    Agregar archivos de proyecto.

---
+++ WebAppLogin/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppLogin.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        #region [Properties]
+        private readonly UsuarioDomain oUsuarioDomain = null;//new UsuarioDatabase();
+        #endregion
+
+        #region [Constructor]
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public UsuarioController()
+        {
+            oUsuarioDomain = new UsuarioDomain();
+        }
+        #endregion
+
+        #region [ActionResult]
+        public IActionResult Index()
+        {
+            var response = oUsuarioDomain.ValidateLogin("fer","123");
+            return View();
+        }
+
+        #endregion
+        
+    }
+}
